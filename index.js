3b635746718f521a7f765d5725ac3c973e9dc247
Alexander <soldatovalex207z@gmail.com>
2023-05-30 18:50:42 +0300

    made good looking

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import Express from "express";
 import bodyParser from "body-parser";
 import dotenv from 'dotenv';
-import fetch from "node-fetch";
 dotenv.config();
 import bookRouter from "./routers/bookRouter.js";
 
@@ -11,51 +10,6 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use('/api', bookRouter);
 
-
-// app.get('/api/books', async (req, res) => {
-//     try {
-//         const amount = req.query.amount;
-
-//         let rawData = await fetch(`https://www.googleapis.com/books/v1/volumes?q=*&printType=books&maxResult=${amount}`);
-//         let googleApiBooks = await rawData.json();
-//         let books = [];
-//         googleApiBooks.items.forEach(element => {
-//             const imgLinks = element.volumeInfo.imageLinks;
-            
-//             books.push({
-//                 id: element.id,
-//                 authors: element.volumeInfo.authors,
-//                 title: element.volumeInfo.title,
-//                 img: imgLinks ? imgLinks : {},
-//                 genre: element.volumeInfo.categories
-//             });
-//         });
-//         res.json(books);   
-//     } catch (error) {
-//         console.log(error);
-//     }
-// });
-
-// app.get('/api/books/:id', async (req, res) => {
-//     try {
-//         let rawData = await fetch(`https://www.googleapis.com/books/v1/volumes/${req.params.id}`);
-//         let bookInfo = await rawData.json();
-        
-//         const imgLinks = bookInfo.volumeInfo.imageLinks ? bookInfo.volumeInfo.imageLinks : {};
-
-//         res.json({
-//             id: bookInfo.id,
-//             authors: bookInfo.volumeInfo.authors,
-//             title: bookInfo.volumeInfo.title,
-//             img: imgLinks,
-//             genre: bookInfo.volumeInfo.categories
-//         });    
-//     } catch (error) {
-//         console.log(error);
-//     }
-// });
-
-
-const listener = app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, () => {
     console.log(`Server listening on port ${process.env.PORT}`);
 })
