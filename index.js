880966559fd0e1e2c5a86c30faf0cb242a1edf23
Alexander <soldatovalex207z@gmail.com>
2023-06-01 19:32:03 +0300

    add cors

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ import bodyParser from "body-parser";
 import dotenv from 'dotenv';
 dotenv.config();
 import bookRouter from "./routers/bookRouter.js";
+import cors from 'cors'
 
 const app = Express();
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL
+}))
 app.use('/api', bookRouter);
 
 app.listen(process.env.PORT, () => {
